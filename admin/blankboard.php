f4713411d1d98497a70f0c4dd3dc485f70b8df38
anjalisaha <anjalisah176@gmail.com>
2022-06-10 18:50:08 +0545

    wordpress theme development

---
+++ admin/blankboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anjali Dashboard</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="/admin/dashboard.css">
+</head>
+<body>
+<!--Top navbar start here-->
+   <?php
+   include 'topnav.php';
+   ?>
+<!--Top navbar end here-->
+
+<!-- Dashboard Body starts -->
+<div class="container-fluid">
+  <div class="row">
+     <!-- Sidebar menu-->
+<?php
+include 'sidenav.php';
+?>
+<!-- Dashboard Body ends -->
+
+    <script type="javascript" src="../js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
